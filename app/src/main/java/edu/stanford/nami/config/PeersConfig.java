3f64f7c20d045d8d3d3d417ecde779f5fb60fa74
Lillian Ma <lillian.ma.ca@gmail.com>
2024-05-19 09:52:43 -0700

    Try to unify RaftConstants with PeersConfig

--- app/src/main/java/edu/stanford/nami/config/PeersConfig.java
+++ app/src/main/java/edu/stanford/nami/config/PeersConfig.java
@@ -1,12 +1,26 @@
 package edu.stanford.nami.config;
 
-import java.util.List;
+import java.util.*;
+
 import lombok.Data;
+import org.apache.ratis.protocol.RaftGroup;
+import org.apache.ratis.protocol.RaftGroupId;
+import org.apache.ratis.protocol.RaftPeer;
 
 /** Connection information for all peers in the cluster. */
 @Data
 public class PeersConfig {
   List<PeerConfig> peers;
+  private static final UUID GROUP_ID = UUID.fromString("02511d47-d67c-49a3-9011-abb3109a44c1");
+
+  public RaftGroup getRaftGroup() {
+    List<RaftPeer> raftPeers = peers.stream().map(peer -> RaftPeer.newBuilder()
+            .setId(peer.getPeerId())
+            .setAddress(peer.getAddress())
+            .setPriority(peer.getPriority())
+            .build()).toList();
+    return RaftGroup.valueOf(RaftGroupId.valueOf(GROUP_ID), raftPeers);
+  }
 
   @Data
   public static class PeerConfig {
@@ -21,5 +35,10 @@ public class PeersConfig {
 
     /** Port where the nami GRPC server is listening. */
     int kvPort;
+
+    /** Used to determine whether this peer should be the leader. The higher the number the more weight. */
+    int priority;
   }
+
+
 }
