08861efca2f7244a8035c1d003815c9ceba1ef42
Juan Tamayo <jtamayo@cs.stanford.edu>
2024-05-24 14:57:46 -0700

    serialize ranges as arrays

--- app/src/main/java/edu/stanford/nami/config/Config.java
+++ app/src/main/java/edu/stanford/nami/config/Config.java
@@ -1,20 +1,44 @@
 package edu.stanford.nami.config;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import edu.stanford.nami.Chunks.ChunkRange;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 public class Config {
+  private static final Gson gson =
+      new GsonBuilder()
+          .registerTypeAdapter(ChunkRange.class, new ChunkRangeDeserializer())
+          .create();
 
   public static <T> T loadConfig(File configFile, String path, Class<T> clazz) {
     var file = configFile.getParentFile().toPath().resolve(path).toFile();
     try (var reader = Files.newReader(file, Charsets.UTF_8)) {
-      return new Gson().fromJson(reader, clazz);
+      return gson.fromJson(reader, clazz);
 
     } catch (IOException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  public static final class ChunkRangeDeserializer implements JsonDeserializer<ChunkRange> {
+
+    @Override
+    public ChunkRange deserialize(
+        JsonElement json, Type typeOfT, JsonDeserializationContext context)
+        throws JsonParseException {
+      var jsonArray = json.getAsJsonArray();
+      Preconditions.checkArgument(jsonArray.size() == 2, "Chunk must be [min, max]");
+      return new ChunkRange(jsonArray.get(0).getAsInt(), jsonArray.get(1).getAsInt());
     }
   }
 }
